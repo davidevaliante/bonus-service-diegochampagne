9662de2345940de925ca65d0aafa6292dd0ee348
davidevaliante <dav.valiante@gmail.com>
2022-10-13 11:43:08 +0200

    removed topads refer

--- pages/index.tsx
+++ pages/index.tsx
@@ -104,7 +104,10 @@ const index: FunctionComponent<Props> = ({ streamerData }) => {
 		<Wrapper>
 			<Container>
 				<div className='top-bar'>
-					<img className='logo' src='/icons/drinkanza modificato.png' />
+					<img
+						className='logo'
+						src='/icons/drinkanza modificato.png'
+					/>
 				</div>
 
 				<h1>
@@ -136,14 +139,16 @@ const index: FunctionComponent<Props> = ({ streamerData }) => {
 				<div style={{ padding: '1rem' }}>
 					<VideoDiscalimer />
 				</div>
-				<div className='bottom'>
-					<p style={{ textAlign: 'center' }}>
-						This service is provided by{' '}
-						<a href='https://www.topaffiliation.com'>
-							Top Affiliation
-						</a>
-					</p>
-				</div>
+				{process.env.REFER === 'true' && (
+					<div className='bottom'>
+						<p style={{ textAlign: 'center' }}>
+							This service is provided by{' '}
+							<a href='https://www.topaffiliation.com'>
+								Top Affiliation
+							</a>
+						</p>
+					</div>
+				)}
 			</Container>
 		</Wrapper>
 	)
